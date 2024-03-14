d2cc0e3204d07dd4dd85e02f996afcddcc8d3b54
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2024-03-13 23:33:44 -0300

    redesign

--- app/page.tsx
+++ app/page.tsx
@@ -2,43 +2,20 @@
 import Image from "next/image";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { FaYoutube, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { NavbarDemo } from "@/components/ui/navbar";
+import { TypewriterEffectDemo } from "@/components/ui/typewritter-effect-demo";
+import { InfiniteMovingCardsDemo } from "@/components/ui/infinite-moving-cards-demo";
 
 export default function Home() {
   return (
+
+
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <BackgroundGradientAnimation>
-        {/* si agrego pointer-events-none , el puntero no sigue las burbujas */}
-        <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-auto text-3xl text-center md:text-5xl lg:text-7xl">
-          <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white to-white/10">
-            ObservDev®
-          </p>
-          <p className="text-xl bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white to-white/20">
-          Desarrollo Web3, Consultoría, Gestión de Proyectos
-          </p>
-          <p className="text-sm bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white to-white/20">
-            Hacemos tus sueños realidad
-          </p>
-          <p className="flex flex-row space-x-1.5 items-center justify-center pt-5">
-            <span
-              onClick={() => window.open("https://www.youtube.com/@observdev", "_blank")}
-              className="border rounded-full p-1.5  border-white/30 hover:border-white pointer-events-auto cursor-pointer">
-              <FaYoutube className="size-6" />
-            </span>
-            <span
-              onClick={() => window.open("https://www.linkedin.com/in/gilberts-ahumada-cabrera-89214086/", "_blank")}
-              className="border rounded-full p-1.5 border-white/30 hover:border-white pointer-events-auto cursor-pointer">
-              <FaLinkedin className="size-6" />
-            </span>
-            <span
-
-              onClick={() => window.open("https://twitter.com/teterabOb", "_blank")}
-              className="border rounded-full p-1.5 border-white/30 hover:border-white pointer-events-auto cursor-pointer">
-              <FaTwitter className="size-6" />
-            </span>
-          </p>
-        </div>
-      </BackgroundGradientAnimation>
-
+  
+        <TypewriterEffectDemo></TypewriterEffectDemo>
+        <InfiniteMovingCardsDemo></InfiniteMovingCardsDemo>
+  
     </main>
+
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
+const {default: flattenColorPalette} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -14,8 +16,14 @@ const config: Config = {
         third: "moveInCircle 40s linear infinite",
         fourth: "moveHorizontal 40s ease infinite",
         fifth: "moveInCircle 20s ease infinite",
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          }
+        },
         moveHorizontal: {
           "0%": {
             transform: "translateX(-50%) translateY(-10%)",
@@ -58,6 +66,18 @@ const config: Config = {
       
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
